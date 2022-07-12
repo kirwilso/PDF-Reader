c85d467602aaeb7559cb821a191e21c906c34c40
kirwilso <105668795+kirwilso@users.noreply.github.com>
2022-07-12 11:27:29 -0500

    attempt tp update my VS code

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 #importing the items I need for the project 
 import tkinter as tk 
 from tkinter import ttk
-from PyPDF2 import PdfReader
+from PyPDF2 import PdfReader, PdfFileReader
 from PIL import Image, ImageTk
 from tkinter.filedialog import askopenfile
 
@@ -26,9 +26,9 @@ instructions.grid(columnspan=3, column=0, row=1)
 
 def open_file():
     browse_text.set("loading...") #this will set the text to "loading" when the user selects a PDF 
-    file = askopenfile(parent=root, mode='rb', title="Choose a file", filetype=[("Pdf file", "*pdf")])
+    file = askopenfile(parent=root, mode='rb', title="Choose a file", filetypes=[("Pdf file", "*.pdf")])
     if file:
-        read_pdf = PyPDF2.PdfFileReader(file)
+        read_pdf = PdfFileReader(file)
         page = read_pdf.getPage(0)
         page_content = page.extractText()
         
@@ -39,19 +39,16 @@ def open_file():
         text_box.tag_configure("center", justify="center") #centering text in the text box
         text_box.tag_add("center", 1.0, "end")
         text_box.grid(column=1, row=3)
-        ##ERROR Note: This text box is not showing yet bc the PDF file cannot be searched for bc PyPDF2 is not working 
+       
 
         browse_text.set("Browse") #this will set the text back to "Browse" after the user has browsed for a PDF 
 
 
-##ERROR Note: All of this is working except the app won't browse for the PDF file because PyPDF2 is still not working in my app..
-#ERROR Note cont: so when I press browse on Tkinter it does not search for the PDF 
 
 
 #browse button 
 browse_text = tk.StringVar()
 browse_btn = tk.Button(root, textvariable=browse_text, command=lambda:open_file(), font="Raleway", bg="blue", highlightbackground='#3E4149', fg="black", height=3, width=15)
-#ERROR having trouble on line above, getting the button to turn blue. bg="blue" I think that this is a mac problem maybe?
 browse_text.set("Browse")
 browse_btn.grid(column=1, row=2)
 
